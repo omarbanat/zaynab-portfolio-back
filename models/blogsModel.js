4df2d93aca0a7440c205534abb8eaa3fdbfbafca
Omar Banat <49315137+omarbanat@users.noreply.github.com>
2023-01-20 12:57:26 +0200

    Update blogsModel.js

--- models/blogsModel.js
+++ models/blogsModel.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const blogsSchema = new Schema({
      title:{type:string,required: true },
      content:{type:string,required: true },
-     image:{url},
+     image:{type:string,required: true },
      publishedDate:{type:Date, required:true}
 })
 
