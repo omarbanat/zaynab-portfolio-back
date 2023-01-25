9b0066d6cffeacc659d6455382a7c155df87e078
BilalSerhal <bilal111.rg@gmail.com>
2023-01-25 10:26:43 +0200

    AllInformationAPI

--- controllers/informationController.js
+++ controllers/informationController.js
@@ -1,5 +1,16 @@
 const informationModel = require('../models/informationModel');
 
+
+//class InfoController {
+// getAll = async (req, res) => {
+//   let infoRecords = await informationModel.find();
+//   res.status(200).json(infoRecords)
+// }
+// getOne = async (req,res)=>{
+//   let record = await 
+// }
+//}
+
 exports.getPrevWorkByID = (req, res) => {
   informationModel.findById(req.params.ID, (err, data) => {
     if (err) {
@@ -8,3 +19,8 @@ exports.getPrevWorkByID = (req, res) => {
     return res.send({ status: 200, data });
   });
 };
+
+exports.getAllInformation = (req, res) => {
+
+}
+
--- routes/informationRoute.js
+++ routes/informationRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPrevWorkByID } = require('../controllers/informationController');
+const { getPrevWorkByID, getAllInformation } = require('../controllers/informationController');
+
+router.get('/', () => {
+    console.log("TEST")
+})
 
 router.get('/getPrevWorkByID/:ID', getPrevWorkByID);
-
+router.get('/getAllInformation', getAllInformation);
 module.exports = router;
